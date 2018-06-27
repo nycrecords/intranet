26c06eba6444960b8239fa1d6dbfe12708ff2afa
Jonathan Yu <jyu@records.nyc.gov>
2018-06-27 14:53:20 -0400

    Added migrations

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,5 @@
 from flask import render_template, redirect, url_for, flash
 from app import db
-from app.models import Sign_In
 from . import main
 from app.main.forms import Sign_In_Form
 from datetime import datetime
@@ -8,43 +7,15 @@ from datetime import datetime
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    form = Sign_In_Form()
+    return render_template('index.html')
 
-    if form.validate_on_submit():
-        for field in form:
-            if field.data is '':
-                field.data = None
-        first_name = form.first_name.data
-        last_name = form.last_name.data
-        affiliation = form.affiliation.data
-        email = form.email.data
-        phone = form.phone.data
-        address = form.address.data
-        city = form.city.data
-        state = form.state.data
-        zipcode = form.zipcode.data
-        country = form.country.data
-        library = form.library.data
-        archives = form.archives.data
-        genealogy = form.genealogy.data
-        timestamp = datetime.now()
 
-        new_user_sign_in = Sign_In(first_name=first_name,
-                                   last_name=last_name,
-                                   affiliation=affiliation,
-                                   email=email,
-                                   phone=phone,
-                                   address=address,
-                                   city=city,
-                                   state=state,
-                                   zipcode=zipcode,
-                                   country=country,
-                                   library=library,
-                                   archives=archives,
-                                   genealogy=genealogy,
-                                   timestamp=timestamp)
-        db.session.add(new_user_sign_in)
-        db.session.commit()
-        flash('Form submitted.')
-        return redirect(url_for('main.index'))
-    return render_template('index.html', form=form)
+@main.route('/news-updates', methods=['GET', 'POST'])
+def news_and_updates():
+    return render_template('news_and_updates.html')
+
+
+@main.route('/news-updates/new', methods=['GET', 'POST'])
+def new_post():
+
+    return render_template('news_and_updates.html')
--- config.py
+++ config.py
@@ -15,18 +15,18 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = (os.environ.get('DEV_DATABASE_URL') or
-                               'postgresql://developer@127.0.0.1:5432/library_archive_form')
+                               'postgresql://developer@127.0.0.1:5432/intranet')
 
 
 class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = (os.environ.get('TEST_DATABASE_URL') or
-                               'postgresql://developer@127.0.0.1:5432/library_archive_form')
+                               'postgresql://developer@127.0.0.1:5432/intranet')
 
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = (os.environ.get('DATABASE_URL') or
-                               'postgresql://developer@127.0.0.1:5432/library_archive_form')
+                               'postgresql://developer@127.0.0.1:5432/intranet')
 
 
 config = {
