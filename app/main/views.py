0f6cea945ba7ee1a56d0cae0197da9106a018ac7
Niko Colon <ncolon@records.nyc.gov>
2018-08-03 15:06:03 -0400

    Added paginate functions, make img oval, style change to the table, and a close button on the popover

--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,10 @@
 from flask import render_template, redirect, url_for, flash, request as flask_request
-from flask_paginate import Pagination, get_page_parameter
 from app import db
 from app.models import Users
 from . import main
 from app.main.forms import Meeting_Notes_Form, Staff_Directory_Search_Form
 from datetime import datetime
 from app.main.utils import create_post
-from sqlalchemy import func
 
 
 @main.route('/', methods=['GET', 'POST'])
@@ -36,25 +34,22 @@ def new_post():
 
 
 
-@main.route('/staff-directory', methods=['GET', 'POST'])
-def staff_directory():
+@main.route('/staff-directory/<int:page_num>', methods=['GET', 'POST'])
+def staff_directory(page_num):
     form = Staff_Directory_Search_Form()
-
+    num=page_num
 
     if form.search.data is "":
-        users = Users.query.all()
+        users = Users.query.order_by(Users.last_name).paginate(per_page=10, page=page_num, error_out=True)
     elif form.filters.data == 'First Name':
-        users = Users.query.filter(Users.first_name.ilike('%'+form.search.data+'%')).all()
+        users = Users.query.filter(Users.first_name.ilike('%'+form.search.data+'%')).paginate(per_page=10, page=page_num, error_out=True)
     elif form.filters.data == 'Last Name':
-        users = Users.query.filter(Users.last_name.ilike('%' + form.search.data + '%')).all()
+        users = Users.query.filter(Users.last_name.ilike('%' + form.search.data + '%')).paginate(per_page=10, page=page_num, error_out=True)
     elif form.filters.data == 'Division':
-        users = Users.query.filter(Users.division.ilike('%'+form.search.data+'%')).all()
+        users = Users.query.filter(Users.division.ilike('%'+form.search.data+'%')).paginate(per_page=10, page=page_num, error_out=True)
     elif form.filters.data == 'Title':
-        users = Users.query.filter(Users.title.ilike('%' + form.search.data + '%')).all()
+        users = Users.query.filter(Users.title.ilike('%' + form.search.data + '%')).paginate(per_page=10, page=page_num, error_out=True)
     else:
-        users = Users.query.order_by(Users.last_name).all()
+        users = Users.query.order_by(Users.last_name).paginate(per_page=10, page=page_num, error_out=True)
 
-    # page = request.args.get(get_page_parameter(), type=int, default=1)
-    # pagination = Pagination(page=page, total=users.count())
-
-    return render_template('staff_directory.html', users=users, form=form)
+    return render_template('staff_directory.html', users=users, form=form, num=num)
