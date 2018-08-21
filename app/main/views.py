fd44d15c1c2a621a3c90f8ad2039fda6bf2fd273
Niko Colon <ncolon@records.nyc.gov>
2018-08-21 15:56:12 -0400

    I added the paginate links, created the search file which outputs the results from the search bar, styled the paginate links and created the view for search file

--- app/main/views.py
+++ app/main/views.py
@@ -39,26 +39,62 @@ def staff_directory():
 
     if form.search.data is "":
         users = Users.query.order_by(Users.last_name).paginate(page=page, per_page=10)
+        next_url = url_for('main.staff_directory', page=users.next_num) \
+            if users.has_next else None
+        prev_url = url_for('main.staff_directory', page=users.prev_num) \
+            if users.has_prev else None
     elif form.filters.data == 'First Name':
-        users = Users.query.filter(Users.first_name.ilike('%' + form.search.data + '%')).paginate(page=page, per_page=10)
-        # users = Users.query.filter(Users.first_name.ilike('%' + form.search.data + '%')).paginate(page=1, per_page=10)
-        # return redirect('/search<string:users>',)
+        return redirect('/search/'+form.filters.data+'/'+form.search.data)
     elif form.filters.data == 'Last Name':
-        users = Users.query.filter(Users.last_name.ilike('%' + form.search.data + '%')).paginate(page=page, per_page=10)
+        return redirect('/search/' + form.filters.data + '/' + form.search.data)
     elif form.filters.data == 'Division':
-        users = Users.query.filter(Users.division.ilike('%' + form.search.data + '%')).paginate(page=page, per_page=10)
+        return redirect('/search/' + form.filters.data + '/' + form.search.data)
     elif form.filters.data == 'Title':
-        users = Users.query.filter(Users.title.ilike('%' + form.search.data + '%')).paginate(page=page, per_page=10)
+        return redirect('/search/' + form.filters.data + '/' + form.search.data)
     else:
         users = Users.query.order_by(Users.last_name).paginate(page=page, per_page=10)
+        next_url = url_for('main.staff_directory', page=users.next_num) \
+            if users.has_next else None
+        prev_url = url_for('main.staff_directory', page=users.prev_num) \
+            if users.has_prev else None
 
-    return render_template('staff_directory.html', users=users, form=form, page=page)
+    return render_template('staff_directory.html', users=users, form=form, page=page, next_url=next_url, prev_url=prev_url)
 
+@main.route('/search/<string:filter>/<string:input>', methods=['GET', 'POST'])
+def show_result(filter,input):
+    form = Staff_Directory_Search_Form()
+    page = flask_request.args.get('page', 1, type=int)
 
-@main.route('/search', methods=['GET','Post'])
-# def show_results(users):
-#     form  = Staff_Directory_Search_Form()
-#     render_template('search.html', users=users, form=form)
+    if filter == 'First Name':
+        users = Users.query.filter(Users.first_name.ilike('%' + input + '%')).paginate(page=page, per_page=10)
+        next_url = url_for('search', filter=filter, input=input, page=users.next_num) \
+            if users.has_next else None
+        prev_url = url_for('search', filter=filter, input=input,  page=users.prev_num) \
+            if users.has_prev else None
+
+    elif filter == 'Last Name':
+        users = Users.query.filter(Users.last_name.ilike('%' + input + '%')).paginate(page=page, per_page=10)
+        next_url = url_for('search', filter=filter, input=input, page=users.next_num) \
+            if users.has_next else None
+        prev_url = url_for('search', filter=filter, input=input, page=users.prev_num) \
+            if users.has_prev else None
+
+    elif filter == 'Division':
+        users = Users.query.filter(Users.division.ilike('%' + input + '%')).paginate(page=page, per_page=10)
+        next_url = url_for('search', filter=filter, input=input, page=users.next_num) \
+            if users.has_next else None
+        prev_url = url_for('search', filter=filter, input=input, page=users.prev_num) \
+            if users.has_prev else None
+
+    elif filter == 'Title':
+        users = Users.query.filter(Users.title.ilike('%' + input + '%')).paginate(page=page, per_page=10)
+        next_url = url_for('search', filter=filter, input=input, page=users.next_num) \
+            if users.has_next else None
+        prev_url = url_for('search', filter=filter, input=input, page=users.prev_num) \
+            if users.has_prev else None
+
+    return render_template('search.html', users=users, form=form, page=page, filter=filter, input=input, next_url=next_url, prev_url=prev_url)
+
 
 @main.route('/get_filter_options_list/<string:filter_value>', methods=['GET'])
 def get_filter_options_list(filter_value):
@@ -67,7 +103,7 @@ def get_filter_options_list(filter_value):
     if filter_value == "First Name":
         for user in users:
             users_array.append(user.first_name)
-        users_array=list(set(users_array))
+        users_array = list(set(users_array))
     if filter_value == "Last Name":
         for user in users:
             if user not in users_array:
