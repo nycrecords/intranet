b50fd3068e273ab2484e310f2af19bfb55143a2a
Niko Colon <ncolon@records.nyc.gov>
2018-09-12 17:18:53 -0400

    Added the reset button and styled the panel heading

--- app/static/js/staff_directory.js
+++ app/static/js/staff_directory.js
@@ -1,47 +1,53 @@
 $(function () {
-    console.log("test");
-    // ON FIRST PAGE load
-    $(document).ready(function () {
-        $.ajax({
-            url: '/get_filter_options_list/' + $('#filter-data').val(),
-            type: "GET",
-            success: function (data) {
-                $("#staff-directory-search-data").autocomplete({
-                    source: data
-                });
-            }
-        });
+  $('#target').popover({
+        content: $('.popover-content'), // set the content to be the 'blah' div
+        placement: 'bottom',
+        html: true
     });
 
-    console.log("test");
-    // handles on filter change
-    $(document).on("focus", "#filter-data", function () {
-        $("#filter-data").off().change(function () {
-            $.ajax({
-                url: '/get_filter_options_list/' + $('#filter-data').val(),
-                type: "GET",
-                success: function (data) {
-                    $("#staff-directory-search-data").autocomplete({
-                        source: data
-                    });
-                }
-            });
-        });
-    });
+    // console.log("test");
+    // // ON FIRST PAGE load
+    // $(document).ready(function () {
+    //     $.ajax({
+    //         url: '/get_filter_options_list/' + $('#filter-data').val(),
+    //         type: "GET",
+    //         success: function (data) {
+    //             $("#staff-directory-search-data").autocomplete({
+    //                 source: data
+    //             });
+    //         }
+    //     });
+    // });
+    //
+    // console.log("test");
+    // // handles on filter change
+    // $(document).on("focus", "#filter-data", function () {
+    //     $("#filter-data").off().change(function () {
+    //         $.ajax({
+    //             url: '/get_filter_options_list/' + $('#filter-data').val(),
+    //             type: "GET",
+    //             success: function (data) {
+    //                 $("#staff-directory-search-data").autocomplete({
+    //                     source: data
+    //                 });
+    //             }
+    //         });
+    //     });
+    // });
 
-
-    $('.popper').popover({
-        placement: 'right',
-        html: true,
-        container: "body",
-        content: function () {
-            return $(this).next('.popper-content').html();
-        }
-    });
-    $(document).on("click", ".popover-content .close", function () {
-        $(this).parents(".popover").popover('hide');
-    });
-    $('[data-toggle="popover"]').on('click', function () {
-        $('[data-toggle=popover]').not(this).popover('hide');
-    });
+    //
+    // $('.popper').popover({
+    //     placement: 'right',
+    //     html: true,
+    //     container: "body",
+    //     content: function () {
+    //         return $(this).next('.popper-content').html();
+    //     }
+    // });
+    // $(document).on("click", ".popover-content .close", function () {
+    //     $(this).parents(".popover").popover('hide');
+    // });
+    // $('[data-toggle="popover"]').on('click', function () {
+    //     $('[data-toggle=popover]').not(this).popover('hide');
+    // });
 });
