bd735f49607dced8eefaee6bcabf915a06de9478
Niko Colon <ncolon@records.nyc.gov>
2018-09-17 16:25:26 -0400

    Fixed some errors in the javascript

--- app/static/js/staff_directory.js
+++ app/static/js/staff_directory.js
@@ -1,49 +1,44 @@
 $(function () {
-  // $('#target').popover({
-  //       content: $('.popover-content'), // set the content to be the 'blah' div
-  //       placement: 'bottom',
-  //       html: true
-  //   });
 
-    // console.log("test");
-    // // ON FIRST PAGE load
-    // $(document).ready(function () {
-    //     $.ajax({
-    //         url: '/get_filter_options_list/' + $('#filter-data').val(),
-    //         type: "GET",
-    //         success: function (data) {
-    //             $("#staff-directory-search-data").autocomplete({
-    //                 source: data
-    //             });
-    //         }
-    //     });
-    // });
-    //
-    // console.log("test");
-    // // handles on filter change
-    // $(document).on("focus", "#filter-data", function () {
-    //     $("#filter-data").off().change(function () {
-    //         $.ajax({
-    //             url: '/get_filter_options_list/' + $('#filter-data').val(),
-    //             type: "GET",
-    //             success: function (data) {
-    //                 $("#staff-directory-search-data").autocomplete({
-    //                     source: data
-    //                 });
-    //             }
-    //         });
-    //     });
-    // });
 
-    //
-    // $('.popper').popover({
-    //     placement: 'right',
-    //     html: true,
-    //     container: "body",
-    //     content: function () {
-    //         return $(this).next('.popper-content').html();
-    //     }
-    // });
+    // ON FIRST PAGE load
+    $(document).ready(function () {
+        $.ajax({
+            url: '/get_filter_options_list/' + $('#filter-data').val(),
+            type: "GET",
+            success: function (data) {
+                $("#staff-directory-search-data").autocomplete({
+                    source: data
+                });
+            }
+        });
+    });
+
+    // handles on filter change
+    $(document).on("focus", "#filter-data", function () {
+        $("#filter-data").off().change(function () {
+            $.ajax({
+                url: '/get_filter_options_list/' + $('#filter-data').val(),
+                type: "GET",
+                success: function (data) {
+                    $("#staff-directory-search-data").autocomplete({
+                        source: data
+                    });
+                }
+            });
+        });
+    });
+
+
+    $('.popper').popover({
+        placement: 'right',
+        html: true,
+        container: "body",
+        content: function () {
+            return $(this).next('.popper-content').html();
+        }
+    });
+    console.log("test");
     // $(document).on("click", ".popover-content .close", function () {
     //     $(this).parents(".popover").popover('hide');
     // });
