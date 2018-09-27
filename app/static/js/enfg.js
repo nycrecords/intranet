b3c70e8b3a181c029331e6061c173b32722e1564
Jonathan Yu <jyu@records.nyc.gov>
2018-09-27 11:34:09 -0400

    Finished ENFG page

--- app/static/js/enfg.js
+++ app/static/js/enfg.js
@@ -6,4 +6,16 @@ $(function () {
         $(this).prop('selected', !$(this).prop('selected'));
         return false;
     });
+
+    // Handle text change when clicking all boroughs button
+    $('#all-boroughs').click(function () {
+        if ($('#all-boroughs').text() === 'All Boroughs') {
+            $('#borough option').prop('selected', true);
+            $('#all-boroughs').text('Remove All')
+        }
+        else {
+            $('#borough option').prop('selected', false);
+            $('#all-boroughs').text('All Boroughs')
+        }
+    });
 });
