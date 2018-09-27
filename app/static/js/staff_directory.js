9d863c7f98e957d90f5ef1a4951c0c87a40d996c
Jonathan Yu <jyu@records.nyc.gov>
2018-09-27 12:15:06 -0400

    Added comments for staff directory

--- app/static/js/staff_directory.js
+++ app/static/js/staff_directory.js
@@ -27,20 +27,36 @@ $(function () {
         });
     });
 
+    // initializes popovers
     $('.popper').popover({
         placement: 'right',
         html: true,
         container: "body",
         content: function () {
-            return $(this).next('.popper-content').html();
+            return $(this).next('.popover-content').html();
         }
     });
 
+    // handles dismissing popovers with close button
     $(document).on("click", ".popover-content .close", function () {
         $(this).parents(".popover").popover('hide');
     });
 
+    // handles dismiss of current popover when you click another popover
     $('[data-toggle="popover"]').on('click', function () {
         $('[data-toggle=popover]').not(this).popover('hide');
+    });
+
+    $('[data-toggle="popover"]').popover();
+
+    // handles dismiss of popover when you click outside
+    $('body').on('click', function (e) {
+        $('[data-toggle="popover"]').each(function () {
+            // the 'is' for buttons that trigger popups
+            // the 'has' for icons within a button that triggers a popup
+            if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+                $(this).popover('hide');
+            }
+        });
     });
 });
