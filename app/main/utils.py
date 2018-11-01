7e04958a92b27f9ec25ace45796497e7f33fa574
Joel Castillo <jocastillo@records.nyc.gov>
2018-11-01 13:17:44 -0400

    Update Queries for Documents
    Now takes deleted files into consideration and returns proper max
    based on original query (maximum of results that match the filters
    provided by the user)

--- app/main/utils.py
+++ app/main/utils.py
@@ -259,25 +259,25 @@ def process_documents_search(document_type_plain_text,
         search_term = search_term.lower()
         # Order the results based on the sort by value
         if sort_by == 'all' or sort_by == 'date_newest':
-            documents = Documents.query.filter(Documents.document_type == document_type_plain_text, Documents.file_title.ilike('%{}%'.format(search_term))).order_by(Documents.last_modified.desc()).slice(documents_start, documents_end)
+            documents = Documents.query.filter(Documents.document_type == document_type_plain_text, Documents.file_title.ilike('%{}%'.format(search_term), Documents.deleted.is_(False))).order_by(Documents.last_modified.desc()).slice(documents_start, documents_end).all()
         elif sort_by == 'name_a_z':
-            documents = Documents.query.filter(Documents.document_type == document_type_plain_text, Documents.file_title.ilike('%{}%'.format(search_term))).order_by(Documents.file_title.asc()).slice(documents_start, documents_end)
+            documents = Documents.query.filter(Documents.document_type == document_type_plain_text, Documents.file_title.ilike('%{}%'.format(search_term), Documents.deleted.is_(False))).order_by(Documents.file_title.asc()).slice(documents_start, documents_end).all()
         elif sort_by == 'name_z_a':
-            documents = Documents.query.filter(Documents.document_type == document_type_plain_text, Documents.file_title.ilike('%{}%'.format(search_term))).order_by(Documents.file_title.desc()).slice(documents_start, documents_end)
+            documents = Documents.query.filter(Documents.document_type == document_type_plain_text, Documents.file_title.ilike('%{}%'.format(search_term), Documents.deleted.is_(False))).order_by(Documents.file_title.desc()).slice(documents_start, documents_end).all()
         elif sort_by == 'date_oldest':
-            documents = Documents.query.filter(Documents.document_type == document_type_plain_text, Documents.file_title.ilike('%{}%'.format(search_term))).order_by(Documents.last_modified.asc()).slice(documents_start, documents_end)
+            documents = Documents.query.filter(Documents.document_type == document_type_plain_text, Documents.file_title.ilike('%{}%'.format(search_term), Documents.deleted.is_(False))).order_by(Documents.last_modified.asc()).slice(documents_start, documents_end).all()
     else:
         if sort_by == 'all' or sort_by == 'date_newest':
-            documents = Documents.query.filter(Documents.document_type == document_type_plain_text).order_by(Documents.last_modified.desc()).slice(documents_start, documents_end)
+            documents = Documents.query.filter(Documents.document_type == document_type_plain_text, Documents.deleted.is_(False)).order_by(Documents.last_modified.desc()).slice(documents_start, documents_end).all()
         elif sort_by == 'name_a_z':
-            documents = Documents.query.filter(Documents.document_type == document_type_plain_text).order_by(Documents.file_title.asc()).slice(documents_start, documents_end)
+            documents = Documents.query.filter(Documents.document_type == document_type_plain_text, Documents.deleted.is_(False)).order_by(Documents.file_title.asc()).slice(documents_start, documents_end).all()
         elif sort_by == 'name_z_a':
-            documents = Documents.query.filter(Documents.document_type == document_type_plain_text).order_by(Documents.file_title.desc()).slice(documents_start, documents_end)
+            documents = Documents.query.filter(Documents.document_type == document_type_plain_text, Documents.deleted.is_(False)).order_by(Documents.file_title.desc()).slice(documents_start, documents_end).all()
         elif sort_by == 'date_oldest':
-            documents = Documents.query.filter(Documents.document_type == document_type_plain_text).order_by(Documents.last_modified.asc()).slice(documents_start, documents_end)
-
+            documents = Documents.query.filter(Documents.document_type == document_type_plain_text, Documents.deleted.is_(False)).order_by(Documents.last_modified.asc()).slice(documents_start, documents_end).all()
+    
     # Get the total number of documents of the specified document type
-    documents_max = Documents.query.filter_by(document_type=document_type_plain_text).count()
+    documents_max = Documents.query.filter(Documents.document_type == document_type_plain_text, Documents.file_title.ilike('%{}%'.format(search_term)), Documents.deleted.is_(False)).count()
     # Create the template for the document type table
     documents_rows = render_template('documents_table.html', document_type=document_type, documents=documents)
 
