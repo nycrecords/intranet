5be58142ee1c6588ada24639aa3bab712f3366eb
Vincent Zheng <vzheng@records.nyc.gov>
2023-08-17 09:55:28 -0400

    Fixed issue with modal not updating on POST and refacting AJAX success code

--- app/static/js/monitor.js
+++ app/static/js/monitor.js
@@ -1,50 +1,37 @@
 // Uses "data" to display information on the table
 function WebsiteMonitorFunction () {
     // "this" cannot be used inside an ajax call.
-    const el = $(this)
+    const websiteID = $(this).attr('id').split('-').pop()
+    const tableRow = $(this)
 
     $.ajax({
         url: '/monitor',
         type: 'POST',
         data: {
-            url: el.find('.website-url').attr('href')
+            url: $('#website-' + websiteID + '-url').attr('href')
         },
         success: function (data) {
-            console.log(data);
-
             // Color the bars on the table.
-            if (data['status_code'] == 200) {
-                el.removeClass("danger")
-                el.addClass("success")
+            if (data['status_code'] === '200') {
+                tableRow.removeClass("danger")
+                tableRow.addClass("success")
 
-                let checkId = el.find('.check').attr('id');
-                $('#' + checkId).show();
-
-                let xId = el.find('.x').attr('id');
-                $('#' + xId).hide();
+                $('#website-' + websiteID + '-check').show();
+                $('#website-' + websiteID + '-x').hide();
             } else {
-                el.removeClass("success")
-                el.addClass("danger")
+                tableRow.removeClass("success")
+                tableRow.addClass("danger")
 
                 // Hide and show the check and x symbol
-                let checkId = el.find('.check').attr('id');
-                $('#' + checkId).hide();
-
-                let xId = el.find('.x').attr('id');
-                $('#' + xId).show();
+                $('#website-' + websiteID + '-check').hide();
+                $('#website-' + websiteID + '-x').show();
             }
 
-            let timeId = el.find('.time').attr('id');
-            $('#' + timeId).html(data["current_timestamp"]);
-            $('#' + timeId).show();
-
-            let timeId2 = el.find('.time-2').attr('id');
-            $('#' + timeId2).html(data["most_recent_success"]);
-            $('#' + timeId2).show();
-
-            let websiteStatusCode = el.find('.status').attr('id');
-            $('#' + websiteStatusCode).html(data["status_code"]);
-            // $('#' + websiteStatusCode).show(data["status-code-success"]);
+            // Inject data
+            $('#website-' + websiteID + '-time').html(data["current_timestamp"]);
+            $('#website-' + websiteID + '-time-2').html(data["most_recent_success"]);
+            $('#website-' + websiteID + '-status-code').html(data["status_code"]);
+            $('#modalBody-' + websiteID).html(data['reason']);
         },
 
         error: function (data) {
@@ -91,7 +78,7 @@ $(document).ready(function () {
 // Create the individual modals.
     $("#monitor-table").find('tr[data-id]').on('click', function () {
         let modalDataID = $(this).data('id')
-        $('#' + 'monitor-modal-' + modalDataID).modal('show');
+        $('#monitor-modal-' + modalDataID).modal('show');
     });
 });
 
